4fdcb2f11d6ff2cb1ebb851781f95c90a5ba998a
PatyPerez7 <patolin0425@gmail.com>
2024-08-05 15:32:08 -0600

    First commit

---
+++ main.js
@@ -0,0 +1,2 @@
+import './src/blackjack' //no es necesario poner el .js; o incluso el index
+import './style.css'
---
+++ src/blackjack/usecases/index.js
@@ -0,0 +1,5 @@
+export { crearDeck } from './crear-deck'; //exportacion con extension "TypeScript Importer"
+export { pedirCarta } from './pedir-carta';
+export { crearCartaHTML } from './crear-carta-html';
+export { turnoComputadora } from './turno-computadora';
+export { valorCarta } from './valor-carta';
